8a506c6a8e267e95b5a98e0d7c327ee71a90c217
emglea <emilyglea@gmail.com>
2023-01-12 10:41:55 -0600

    added some comments explaining directives, selectors, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // The selector instructs Angular to instantiate the AppComponent whenever it finds a corresponding app-root tag
+  // the app-root tag <app-root></app-root> is in index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // this is displayed using {{}} interpolation in app.component.html
   title = 'List';
 }
